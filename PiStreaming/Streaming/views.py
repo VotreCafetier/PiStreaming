41442efdf0a88d037b14128a57446b8d1e0211c7
Dany Gauthier <danygauthier57@yahoo.ca>
2021-08-02 18:07:33 -0400

    Added virtual environment

--- PiStreaming/Streaming/views.py
+++ PiStreaming/Streaming/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from Streaming.video import *
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+
+def live(request):
+    frame = livefeed.update()
+    context = {frame}
+    return render(request, 'live.html', context)

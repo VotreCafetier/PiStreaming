0f43ae5f5e9dc0aeb4f44c56c4fb1adf4109e860
Dany Gauthier <danygauthier57@yahoo.ca>
2021-08-06 17:06:06 -0400

    Added a cv2 + picamera prototype

--- PiStreaming/Streaming/video_pi.py
+++ PiStreaming/Streaming/video_pi.py
@@ -1,23 +1,30 @@
 import picamera
-import time
+import numpy as np
+import cv2
+
+with picamera.PiCamera() as camera:
 
 class PiCameraStreaming(object):
-    def __init__(self, camera_res, camera_framerate, camera_brightness, camera_exposure_mode):
-        self.camera = PiCamera()
-        self.camera_res = camera_res
-        self.camera_framerate = camera_framerate
-        self.camera_brightness = camera_brightness
-        self.camera_exposuremode = camera_exposure_mode
+    def __init__(self):
+        self.camera = picamera.PiCamera()
+        self.camera_res = (320, 240)
+        self.camera_framerate = 24
+        # self.camera_brightness = camera_brightness
+        # self.camera_exposuremode = camera_exposure_mode
 
     def __del__(self):
         self.camera.close()
+        self.video.release()
+        cv2.destroyAllWindows()
 
     def get_frame(self):
-        with picamera.PiCamera() as camera:
-            camera.start_preview()
-            # Camera warm-up time
-            time.sleep(2)
-            return camera.capture(my_stream, 'jpeg')
+        camera.resolution = self.camera_res
+        camera.framerate = self.camera_framerate
+        image = np.empty((self.camera_res[0] * self.camera_res[1] * 3,), dtype=np.uint8)
+        camera.capture(image, 'bgr')
+        image = image.reshape((self.camera_res[0], self.camera_res[1], 3))
+        ret,jpeg = cv2.imencode('.jpg',image)
+        return jpeg.tobytes()
 
 
 def get_picam(camera):
